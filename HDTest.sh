73cd351b8c27fdb56110ee6e40cc561177f9dd9d
Joshua Charles <joshcharlestechie@gmail.com>
2015-06-09 12:08:05 -0400

    Fixed comments

--- HDTest.sh
+++ HDTest.sh
@@ -2,28 +2,30 @@
 
 #Runs through SmartCtl Hard Drive Test
 
-#
-echo smartctl -i $1 | awk -F: '($1 ~ /Serials/){ print $2 } ' 
 
+#Makes the Serial Number the File Header 
+serial=`smartctl -i $1 | awk -F: '($1 ~ /Serial/){ print $2 } '` 
+smartctl -i $1 >> $serial.txt
+echo $serial
 
 #Does a Health Test
-smartctl -H $1 >> smartctl.txt
+smartctl -H $1 >> $serial.txt
 
+#Runs SHORT tests 
 smartctl -t short $1
 echo
 echo Wait until first test is done then press [Enter]
-read line
 
+read line
 
 smartctl -t short $1
 echo
 echo Second test is starting wait until complete
+
 read line2
 
 #Outputs the results of the test
-smartctl -l selftest $1 >> smartctl.txt
+smartctl -l selftest $1 >> $serial.txt
 
 #Shows final output
-cat smartctl.txt
-
-#rm smartctl.txt
+cat $serial.txt
